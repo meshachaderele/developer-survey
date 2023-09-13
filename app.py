9d04aa62f953cd62ff9de6d14cdd8e71d767c317
meshachaderele <meshachaderele@gmail.com>
2023-09-13 06:51:17 +0200

    my first deployment

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from predict_page import show_predict_page
+from explore_page import show_explore_page
+
+
+page = st.sidebar.selectbox("Explore Or Predict", ("Predict", "Explore"))
+
+if page == "Predict":
+    show_predict_page()
+else:
+    show_explore_page()
+    
+
+# Define the footer content
+footer_content = """
+<hr>
+<p style="text-align:center">Made with ❤️ by Meshach Aderele</p>
+"""
+
+# Add the footer
+st.markdown(footer_content, unsafe_allow_html=True)
